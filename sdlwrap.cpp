b5273d4d629811144a031b5a8f1f56c5d4acf1f4
ts14ic <iw2snl@gmail.com>
2016-05-01 18:30:17 +0300

    Move sdl to it's own shared header

---
+++ sdlwrap.cpp
@@ -0,0 +1,70 @@
+#include "sdlwrap.h"
+#include <SDL.h>
+#include <SDL_image.h>
+
+int& screenWidth() {
+    static int ret = 800;
+    return ret;
+}
+
+int& screenHeight() {
+    static int ret = 600;
+    return ret;
+}
+
+SDL_Renderer*& renderer() {
+    static SDL_Renderer* ret = nullptr;
+    return ret;
+}
+
+SDL_Window*& window() {
+    static SDL_Window* ret = nullptr;
+    return ret;
+}
+
+bool init_sdl() {
+    int SDL_flags = SDL_INIT_VIDEO | SDL_INIT_TIMER;
+    if(0 != SDL_Init(SDL_flags)) {
+        SDL_Log("Failed SDL_Init. %s.\n", SDL_GetError());
+        return false;
+    }
+    window() = SDL_CreateWindow(
+        "TopDown - Reborn",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        screenWidth(), screenHeight(),
+        SDL_WINDOW_SHOWN
+    );
+    if(!window()) {
+        SDL_Log("Failed window creation. %s.\n", SDL_GetError());
+        return false;
+    }
+    
+    renderer() = SDL_CreateRenderer(
+        window(), -1,
+        SDL_RENDERER_ACCELERATED |
+        SDL_RENDERER_PRESENTVSYNC
+    );
+    if(!renderer()) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Can't create renderer.\n%s\n", SDL_GetError());
+        return false;
+    }
+    
+    int IMG_flags = IMG_INIT_JPG | IMG_INIT_PNG;
+    if(IMG_flags != (IMG_Init(IMG_flags) & IMG_flags)) {
+        SDL_Log("Failed IMG_Init. %s.\n", IMG_GetError());
+        return false;
+    }
+    
+    SDL_SetRenderDrawColor(renderer(), 0x10, 0x10, 0x10, 0xff);
+    SDL_RenderClear(renderer());
+    SDL_RenderPresent(renderer());
+    
+    SDL_Log("init_sdl(): fine");
+    return true;
+}
+
+void close_sdl() {
+    SDL_Quit();
+}
---
+++ sdlwrap.h
@@ -0,0 +1,21 @@
+#ifndef SDLWRAP_H_INCLUDED
+#define SDLWRAP_H_INCLUDED
+
+/* forward declarations begin */
+struct SDL_Renderer;
+struct SDL_Window;
+/* forward declarations end */
+
+unsigned const maxFps = 60;
+unsigned const minSpf = 1000/maxFps; // 16.6 milliseconds per frame
+
+int& screenWidth();
+int& screenHeight();
+
+SDL_Renderer*& renderer();
+SDL_Window*& window();
+
+bool init_sdl();
+void close_sdl();
+
+#endif // SDLWRAP_H_INCLUDED
