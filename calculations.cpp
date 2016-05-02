a2eadc33bb90bec3646d32d874f71a125d98e61c
ts14ic <iw2snl@gmail.com>
2016-05-02 18:34:02 +0300

    Added some calculations helpers

---
+++ calculations.cpp
@@ -0,0 +1,87 @@
+#include "calculations.h"
+#include <cstdlib>
+#include <cmath>
+#include <SDL_rect.h>
+
+float pi() {
+    return 3.14159265359f;
+}
+
+float rsin(float x) {
+    return std::sin(x);
+}
+
+float rcos(float x) {
+    return std::cos(x);
+}
+
+float dsin(float x) {
+    return std::sin(to_rad(x));
+}
+
+float dcos(float x) {
+    return std::cos(to_rad(x));
+}
+
+float to_rad(float angle) {
+    return angle * pi()/180.f;
+}
+
+float to_deg(float angle) {
+    return angle * 180.f/pi();
+}
+
+float rand_float(float range) {
+    float k = (float) rand() / RAND_MAX;
+    return range * (k * 2 - 1);
+}
+
+float get_distance(int ax, int ay, int bx, int by) {
+    return std::hypot(bx - ax, by - ay);
+}
+
+float get_distance(SDL_Point const& a, SDL_Point const& b) {
+    return get_distance(a.x, a.y, b.x, b.y);
+}
+
+float get_angle(int ax, int ay, int bx, int by) {
+    return std::atan2(by - ay, bx - ax);
+}
+
+float get_angle(SDL_Point const& a, SDL_Point const& b) {
+    return get_angle(a.x, a.y, b.x, b.y);
+}
+
+bool collides(SDL_Rect const& a, SDL_Rect const& b) {
+    return SDL_HasIntersection(&a, &b);
+}
+
+bool collides(Circle const& a, SDL_Rect const& b) {
+    SDL_Point c;
+
+    if(a.x < b.x){
+        c.x = b.x;
+    }else if(a.x > b.x + b.w){
+        c.x = b.x + b.w;
+    }else{
+        c.x = a.x;
+    }
+
+    if(a.y < b.y){
+        c.y = b.y;
+    }else if(a.y > b.y + b.w){
+        c.y = b.y + b.w;
+    }else{
+        c.y = a.y;
+    }
+
+    return get_distance(a.x, a.y, c.x, c.y) < a.r;
+}
+
+bool collides(SDL_Rect const& a, Circle const& b) {
+    return collides(b, a);
+}
+
+bool collides(Circle const& a, Circle const& b) {
+    return get_distance(a.x, a.y, b.x, b.y) < (a.r + b.r);
+}
---
+++ calculations.h
@@ -0,0 +1,41 @@
+#ifndef CALCULATIONS_H_INCLDUDED
+#define CALCULATIONS_H_INCLDUDED
+
+/* forward declarations begin */
+struct SDL_Point;
+struct SDL_Rect;
+/* forward declarations end */
+
+struct Circle {
+    Circle() = default;
+    Circle(int x, int y, int r) : x(x), y(y), r(r) {}
+    
+    int x, y;
+    int r;
+};
+
+// trigonometry
+float pi();
+
+float to_rad(float angle);
+float to_deg(float angle);
+
+float rcos(float x);
+float rsin(float x);
+float dcos(float x);
+float dsin(float x);
+
+float rand_float(float range);
+
+float get_distance(int ax, int ay, int bx, int by);
+float get_distance(SDL_Point const& a, SDL_Point const& b);
+
+float get_angle(int ax, int ay, int bx, int by);
+float get_angle(SDL_Point const& a, SDL_Point const& b);
+
+bool collides(SDL_Rect const& a, SDL_Rect const& b);
+bool collides(Circle const& a, SDL_Rect const& b);
+bool collides(SDL_Rect const& a, Circle const& b);
+bool collides(Circle const& a, Circle const& b);
+
+#endif // CALCULATIONS_H_INCLDUDED
