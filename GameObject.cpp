ea402f630075e3760a19969254124517ed876fc0
ts14ic <iw2snl@gmail.com>
2016-05-05 22:07:22 +0300

    Fix health bar being displayed when health is negative

--- GameObject.cpp
+++ GameObject.cpp
@@ -3,11 +3,13 @@
 #include <SDL_render.h>
 
 void Damageable::default_render_health(SDL_Color const& c) const {
-    SDL_Rect healthRect;
-    healthRect.w = 50.f * hp() / defaultHp();
-    healthRect.h = 5;
-    healthRect.x = x() - healthRect.w / 2;
-    healthRect.y = y() - circle().r;
-    SDL_SetRenderDrawColor(renderer(), c.r, c.g, c.b, c.a);
-    SDL_RenderFillRect(renderer(), &healthRect);
+    if(hp() > 0) {
+        SDL_Rect healthRect;
+        healthRect.w = 50.f * hp() / defaultHp();
+        healthRect.h = 5;
+        healthRect.x = x() - healthRect.w / 2;
+        healthRect.y = y() - circle().r;
+        SDL_SetRenderDrawColor(renderer(), c.r, c.g, c.b, c.a);
+        SDL_RenderFillRect(renderer(), &healthRect);
+    }
 }
