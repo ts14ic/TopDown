42535d753662bac110061b0a3d0244e6d6a84ea6
Artyom Yazinsky <iw2snl@gmail.com>
2018-09-01 19:39:18 +0300

    Split declarations and implementations for json parsing functions

--- src/io/json/get_value.h
+++ src/io/json/get_value.h
@@ -19,45 +19,51 @@ namespace json {
     };
 
     namespace detail {
-        const rapidjson::Value* get_value(
-                const rapidjson::Value& root,
-                const char* source
-        );
+        const rapidjson::Value* get_value(const rapidjson::Value& root, const char* source);
 
         template <class T>
-        T value_cast(const rapidjson::Value* value, const char* source) {
-            // Despite the docs saying Is<> and Get<> work only on scalars,
-            // this also works for ConstArray and ConstObject.
-            if (!value->Is<T>()) {
-                throw ParseException{
-                        std::string{"Value on "} + source + " is not of type " +
-                        utils::demangle_type_name(typeid(T).name())
-                };
-            }
-            return value->Get<T>();
-        }
+        T get_value_casted(const rapidjson::Value& root, const char* source);
     }
 
     template <typename T>
-    T get_value(const rapidjson::Value& root, const char* source = SOURCE_SELF) {
-        auto value = detail::get_value(root, source);
-        return detail::value_cast<T>(value, source);
-    }
+    T get_value(const rapidjson::Value& root, const char* source = SOURCE_SELF);
 
     rapidjson::Value::ConstObject get_object(const rapidjson::Value& root, const char* path = SOURCE_SELF);
 
     const char* get_czstring(const rapidjson::Value& root, const char* path = SOURCE_SELF);
 
     template <typename T>
-    std::vector<T> get_values(const rapidjson::Value& root, const char* source = SOURCE_SELF) {
-        auto array_value = detail::get_value(root, source);
-        auto array = detail::value_cast<rapidjson::Value::ConstArray>(array_value, source);
+    std::vector<T> get_values(const rapidjson::Value& root, const char* source = SOURCE_SELF);
+}
 
-        std::vector<T> vector;
-        vector.reserve(array.Size());
-        for (const auto& value : array) {
-            vector.emplace_back(value.Get<T>());
-        }
-        return vector;
+template <class T>
+T json::detail::get_value_casted(const rapidjson::Value& root, const char* source) {
+    auto value = get_value(root, source);
+
+    // Despite the docs saying Is<> and Get<> work only on scalars,
+    // this also works for ConstArray and ConstObject.
+    if (!value->Is<T>()) {
+        using namespace std::string_literals;
+        throw ParseException{
+                "Value on "s + source + " is not of type " + utils::demangle_type_name(typeid(T).name())
+        };
     }
+    return value->Get<T>();
+}
+
+template <typename T>
+T json::get_value(const rapidjson::Value& root, const char* source) {
+    return detail::get_value_casted<T>(root, source);
+}
+
+template <typename T>
+std::vector<T> json::get_values(const rapidjson::Value& root, const char* source) {
+    auto array = detail::get_value_casted<rapidjson::Value::ConstArray>(root, source);
+
+    std::vector<T> vector;
+    vector.reserve(array.Size());
+    for (const auto& value : array) {
+        vector.emplace_back(value.Get<T>());
+    }
+    return vector;
 }
