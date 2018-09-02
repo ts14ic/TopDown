12b9e8c05af649e3d4a611968c1413970bc91cc8
Artyom Yazinsky <iw2snl@gmail.com>
2018-09-02 15:35:47 +0300

    Add HasSpeed mixin

--- src/game/object/GameObject.h
+++ src/game/object/GameObject.h
@@ -1,42 +1,13 @@
 #pragma once
 
 #include "HasTransform.h"
-#include <game/components/Speed.h>
+#include "HasSpeed.h"
 #include <engine/graphic/Graphic.h>
 #include <iosfwd>
 
-class GameObject : public HasTransform {
+class GameObject : public HasTransform, public HasSpeed {
 public:
-    virtual Speed get_speed() const = 0;
-
-    virtual void set_speed(Speed speed) = 0;
-
     virtual std::string get_tex_name() const = 0;
-
-    float get_max_movement_speed() const {
-        return get_speed().max_speed;
-    }
-
-    void set_max_movement_speed(float max_speed) {
-        Speed speed = get_speed();
-        speed.max_speed = max_speed;
-        set_speed(speed);
-    }
-
-    Speed2<float> get_current_speed() const {
-        return get_speed().current_speed;
-    }
-
-    void set_current_speed(Speed2<float> current_speed) {
-        Speed speed = get_speed();
-        speed.current_speed = current_speed;
-        set_speed(speed);
-    }
-
-    void set_current_speed(float x, float y) {
-        set_current_speed(make_speed(x, y));
-    }
-
 protected:
     void default_move();
 
