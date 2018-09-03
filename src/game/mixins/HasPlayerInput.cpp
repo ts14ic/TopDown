df550ddf571b410464ab4a529d9badcd84c0b3bc
Artyom Yazinsky <iw2snl@gmail.com>
2018-09-04 00:23:00 +0300

    Provide the control scheme using the config json

--- src/game/mixins/HasPlayerInput.cpp
+++ src/game/mixins/HasPlayerInput.cpp
@@ -1,9 +1,45 @@
 #include "HasPlayerInput.h"
-#include <unordered_map>
+#include <io/files/files.h>
+#include <io/json/json.h>
+#include <iostream>
 
-PlayerInput::HoldAction event_to_hold_action(const KeyboardEvent& event);
+HasPlayerInput::HasPlayerInput(const char* control_scheme_file_name) {
+    auto file = files::read_file_to_string(control_scheme_file_name);
+    auto document = json::parse_json(file);
 
-PlayerInput::QuickAction event_to_quick_action(const KeyboardEvent& event);
+    _key_to_hold_action[json::get_int(document, "/controls/hold/up")] = PlayerInput::HOLD_UP;
+    _key_to_hold_action[json::get_int(document, "/controls/hold/left")] = PlayerInput::HOLD_LEFT;
+    _key_to_hold_action[json::get_int(document, "/controls/hold/down")] = PlayerInput::HOLD_DOWN;
+    _key_to_hold_action[json::get_int(document, "/controls/hold/right")] = PlayerInput::HOLD_RIGHT;
+    _key_to_hold_action[json::get_int(document, "/controls/hold/trigger")] = PlayerInput::HOLD_TRIGGER;
+
+    _key_to_quick_action[json::get_int(document, "/controls/quick/0")] = PlayerInput::QUICK_0;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/1")] = PlayerInput::QUICK_1;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/2")] = PlayerInput::QUICK_2;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/3")] = PlayerInput::QUICK_3;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/4")] = PlayerInput::QUICK_4;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/5")] = PlayerInput::QUICK_5;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/6")] = PlayerInput::QUICK_6;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/7")] = PlayerInput::QUICK_7;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/8")] = PlayerInput::QUICK_8;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/9")] = PlayerInput::QUICK_9;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/next_weapon")] = PlayerInput::QUICK_NEXT_WEAPON;
+    _key_to_quick_action[json::get_int(document, "/controls/quick/previous_weapon")] = PlayerInput::QUICK_PREVIOUS_WEAPON;
+}
+
+PlayerInput::HoldAction HasPlayerInput::event_to_hold_action(const KeyboardEvent& event) const {
+    auto action = _key_to_hold_action.find(event.get_key());
+    return action != _key_to_hold_action.end()
+           ? action->second
+           : PlayerInput::HOLD_NONE;
+}
+
+PlayerInput::QuickAction HasPlayerInput::event_to_quick_action(const KeyboardEvent& event) const {
+    auto action = _key_to_quick_action.find(event.get_key());
+    return action != _key_to_quick_action.end()
+           ? action->second
+           : PlayerInput::QUICK_NONE;
+}
 
 void HasPlayerInput::handle_key_event(const KeyboardEvent& event) {
     if (KeyboardEvent::is_key_down(event)) {
@@ -12,45 +48,6 @@ void HasPlayerInput::handle_key_event(const KeyboardEvent& event) {
         release(event_to_hold_action(event));
         tap(event_to_quick_action(event));
     }
-}
-
-PlayerInput::HoldAction event_to_hold_action(const KeyboardEvent& event) {
-    static std::unordered_map<int, PlayerInput::HoldAction> ACTIONS_MAP{
-            {KEY_UP, PlayerInput::HOLD_UP},
-            {'w', PlayerInput::HOLD_UP},
-            {KEY_DOWN, PlayerInput::HOLD_DOWN},
-            {'s', PlayerInput::HOLD_DOWN},
-            {KEY_LEFT, PlayerInput::HOLD_LEFT},
-            {'a', PlayerInput::HOLD_LEFT},
-            {KEY_RIGHT, PlayerInput::HOLD_RIGHT},
-            {'d', PlayerInput::HOLD_RIGHT},
-            {KEY_SPACE, PlayerInput::HOLD_TRIGGER}
-    };
-    auto action = ACTIONS_MAP.find(event.get_key());
-    return action != ACTIONS_MAP.end()
-           ? action->second
-           : PlayerInput::HOLD_NONE;
-}
-
-PlayerInput::QuickAction event_to_quick_action(const KeyboardEvent& event) {
-    static std::unordered_map<int, PlayerInput::QuickAction> ACTIONS_MAP{
-            {'0', PlayerInput::QUICK_0},
-            {'1', PlayerInput::QUICK_1},
-            {'2', PlayerInput::QUICK_2},
-            {'3', PlayerInput::QUICK_3},
-            {'4', PlayerInput::QUICK_4},
-            {'5', PlayerInput::QUICK_5},
-            {'6', PlayerInput::QUICK_6},
-            {'7', PlayerInput::QUICK_7},
-            {'8', PlayerInput::QUICK_8},
-            {'9', PlayerInput::QUICK_9},
-            {',', PlayerInput::QUICK_PREVIOUS_WEAPON},
-            {'.', PlayerInput::QUICK_NEXT_WEAPON},
-    };
-    auto action = ACTIONS_MAP.find(event.get_key());
-    return action != ACTIONS_MAP.end()
-           ? action->second
-           : PlayerInput::QUICK_NONE;
 }
 
 void HasPlayerInput::handle_mouse_event(const MouseScrollEvent& event) {
--- src/game/mixins/HasPlayerInput.h
+++ src/game/mixins/HasPlayerInput.h
@@ -3,10 +3,13 @@
 #include <game/components/PlayerInput.h>
 #include <engine/input/MouseEventHandler.h>
 #include <engine/input/KeyboardEventHandler.h>
+#include <unordered_map>
 
 class HasPlayerInput : public KeyboardEventHandler,
                        public MouseEventHandler {
 public:
+    explicit HasPlayerInput(const char* control_scheme_file_name = "data/config.json");
+
     virtual PlayerInput get_player_input() const = 0;
 
     virtual void set_player_input(PlayerInput input) = 0;
@@ -18,6 +21,10 @@ public:
     void handle_mouse_event(const MousePointEvent& event) override;
 
 private:
+    PlayerInput::HoldAction event_to_hold_action(const KeyboardEvent& event) const;
+
+    PlayerInput::QuickAction event_to_quick_action(const KeyboardEvent& event) const;
+
     void press(PlayerInput::HoldAction action);
 
     void release(PlayerInput::HoldAction action);
@@ -25,4 +32,7 @@ private:
     void tap(PlayerInput::QuickAction action);
 
     void move_mouse(Point2<int> mouse_position);
+
+    std::unordered_map<int, PlayerInput::HoldAction> _key_to_hold_action;
+    std::unordered_map<int, PlayerInput::QuickAction> _key_to_quick_action;
 };
