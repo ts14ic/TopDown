5db6e360838435f947342c8a9016870cc0c895ca
Artyom Yazinsky <iw2snl@gmail.com>
2018-11-20 20:08:14 +0200

    Reformat

--- src/game/components/ZombieAnimation.cpp
+++ src/game/components/ZombieAnimation.cpp
@@ -1,13 +1,19 @@
 #include "ZombieAnimation.h"
 
 const ZombieAnimation::Animation ZombieAnimation::MOVING{
-        "zombie", /*frame speeds: none*/{}, AnimationType::STATIC
+        "zombie",
+        /*frame speeds: none*/{},
+        AnimationType::STATIC
 };
 const ZombieAnimation::Animation ZombieAnimation::ATTACKING{
-        "zombie_attack", {150, 125, 100, 75, 50, 25}, AnimationType::REPEATABLE
+        "zombie_attack",
+        /*frame speeds*/{150, 125, 100, 75, 50, 25},
+        AnimationType::REPEATABLE
 };
 const ZombieAnimation::Animation ZombieAnimation::DYING{
-        "zombie_death", {75, 100, 130, 160, 190, 210, 240, 300}, AnimationType::ONESHOT
+        "zombie_death",
+        /*frame speeds*/{75, 100, 130, 160, 190, 210, 240, 300},
+        AnimationType::ONESHOT
 };
 
 bool operator==(const ZombieAnimation::Animation& lhs, const ZombieAnimation::Animation& rhs) {
