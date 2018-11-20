26dd9fd0b19d554860dc417071081911a62095a2
Artyom Yazinsky <iw2snl@gmail.com>
2018-11-20 20:07:37 +0200

    Specify some different speeds for frames

--- src/game/components/ZombieAnimation.cpp
+++ src/game/components/ZombieAnimation.cpp
@@ -4,10 +4,10 @@ const ZombieAnimation::Animation ZombieAnimation::MOVING{
         "zombie", /*frame speeds: none*/{}, AnimationType::STATIC
 };
 const ZombieAnimation::Animation ZombieAnimation::ATTACKING{
-        "zombie_attack", std::vector<std::size_t>(/*frames*/6, /*speed*/100), AnimationType::REPEATABLE
+        "zombie_attack", {150, 125, 100, 75, 50, 25}, AnimationType::REPEATABLE
 };
 const ZombieAnimation::Animation ZombieAnimation::DYING{
-        "zombie_death", std::vector<std::size_t>(/*frames*/7, /*speed*/500), AnimationType::ONESHOT
+        "zombie_death", {75, 100, 130, 160, 190, 210, 240, 300}, AnimationType::ONESHOT
 };
 
 bool operator==(const ZombieAnimation::Animation& lhs, const ZombieAnimation::Animation& rhs) {
