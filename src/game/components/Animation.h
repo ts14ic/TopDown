9ee5ef5f6fed5816e22e7e2692682ede1f475162
Artyom Yazinsky <iw2snl@gmail.com>
2018-11-20 22:01:31 +0200

    Separate ZombieAnimation into Animation and Preset

---
+++ src/game/components/Animation.h
@@ -0,0 +1,77 @@
+#include <utility>
+
+#pragma once
+
+#include <game/timer/Timer.h>
+#include <string>
+#include <vector>
+
+namespace animation {
+    enum class AnimationType {
+        STATIC,
+        ONESHOT,
+        REPEATABLE,
+    };
+
+    struct Preset {
+        std::string name;
+        std::vector<std::size_t> frame_durations;
+        AnimationType type = AnimationType::REPEATABLE;
+    };
+
+    extern const Preset ZOMBIE_MOVING;
+    extern const Preset ZOMBIE_ATTACKING;
+    extern const Preset ZOMBIE_DYING;
+}
+
+class Animation {
+public:
+    void set_preset(animation::Preset preset) {
+        _current_preset = std::move(preset);
+        _frame = 0;
+        _timer.restart();
+    }
+
+    std::string get_tex_name() const {
+        if (_current_preset.type == animation::AnimationType::STATIC) {
+            return _current_preset.name;
+        }
+        return _current_preset.name + std::to_string(_frame);
+    }
+
+    bool is_last_frame() const {
+        return _frame + 1 >= _current_preset.frame_durations.size();
+    }
+
+    void next_frame() {
+        if (_current_preset.type == animation::AnimationType::STATIC) {
+            return;
+        }
+        if (is_last_frame()) {
+            if (_current_preset.type == animation::AnimationType::REPEATABLE) {
+                _frame = 0;
+            }
+        } else {
+            ++_frame;
+        }
+    }
+
+    void forward_time() {
+        if (_current_preset.type == animation::AnimationType::STATIC) {
+            return;
+        }
+        if (_timer.ticks_passed_since_start(_current_preset.frame_durations[_frame])) {
+            next_frame();
+            _timer.restart();
+        }
+    }
+
+private:
+    std::size_t _frame = 0;
+    Timer _timer;
+    animation::Preset _current_preset = animation::ZOMBIE_MOVING;
+};
+
+bool operator==(const animation::Preset& lhs, const animation::Preset& rhs);
+
+bool operator!=(const animation::Preset& lhs, const animation::Preset& rhs);
