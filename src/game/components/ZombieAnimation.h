fc382c61619a75df901a4753709e2d2cbbb1cc5b
Artyom Yazinsky <iw2snl@gmail.com>
2018-11-20 19:26:47 +0200

    Simplify the animation ticking logic a bit

--- src/game/components/ZombieAnimation.h
+++ src/game/components/ZombieAnimation.h
@@ -26,8 +26,8 @@ public:
 
     void set_animation(Animation animation, const Clock& clock) {
         _current = std::move(animation);
-        reset_frame();
-        reset_timer(clock);
+        _frame = 0;
+        _timer.restart(clock);
     }
 
     std::string get_tex_name() const {
@@ -37,47 +37,32 @@ public:
         return _current.name + std::to_string(_frame);
     }
 
-    int get_frames_count() const {
-        return _current.frames;
-    }
-
-    int get_frame() const {
-        return _frame;
-    }
-
     bool is_last_frame() const {
-        return _frame == _current.frames - 1;
+        return _frame + 1 == _current.frames;
     }
 
     void next_frame() {
-        ++_frame;
-    }
+        if (_current.animation_type == AnimationType::STATIC) {
+            return;
+        }
+        if (!is_last_frame()) {
+            ++_frame;
 
-    void reset_frame() {
-        _frame = 0;
-    }
-
-    void tick(const Clock& clock) {
-        if (_current.animation_type == AnimationType::REPEATABLE) {
-            if (_timer.ticks_passed_since_start(clock, _current.speed)) {
-                next_frame();
-                if (get_frame() >= get_frames_count()) {
-                    reset_frame();
-                }
-                reset_timer(clock);
-            }
-        } else if (_current.animation_type == AnimationType::ONESHOT) {
-            if (_timer.ticks_passed_since_start(clock, _current.speed)) {
-                if (_frame < get_frames_count()) {
-                    next_frame();
-                    reset_timer(clock);
-                }
-            }
+        } // Else we are on last frame. If it's a repeatable animation...
+        else if (_current.animation_type == REPEATABLE) {
+            // ... reset the frame
+            _frame = 0;
         }
     }
 
-    void reset_timer(const Clock& clock) {
-        _timer.restart(clock);
+    void tick(const Clock& clock) {
+        if (_current.animation_type == AnimationType::STATIC) {
+            return;
+        }
+        if (_timer.ticks_passed_since_start(clock, _current.speed)) {
+            next_frame();
+            _timer.restart(clock);
+        }
     }
 
 private:
