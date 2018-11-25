f4286abb89a2d497d51dbdcfeaf45da45533c658
Artyom Yazinsky <iw2snl@gmail.com>
2018-11-25 23:41:53 +0200

    Remove unused method

--- src/game/object/Damageable.cpp
+++ src/game/object/Damageable.cpp
@@ -1,15 +1,2 @@
 #include "Damageable.h"
 
-void Damageable::default_render_health(Graphic& graphic, const Color& color, float frames_count) const {
-    if (has_hp()) {
-        Box health_box;
-
-        health_box.set_size(50.f * get_hp() / get_default_hp(), 5.f);
-        health_box.set_left_top(
-                get_position().x - health_box.get_width() / 2 + get_current_speed().x * frames_count,
-                get_position().y - get_circle().get_radius() + get_current_speed().y * frames_count
-        );
-
-        graphic.render_box(health_box, color);
-    }
-}
--- src/game/object/Damageable.h
+++ src/game/object/Damageable.h
@@ -10,7 +10,4 @@ public:
     virtual void take_damage(int damageAmount) = 0;
 
     virtual bool is_dead() const = 0;
-
-protected:
-    void default_render_health(Graphic& graphic, const Color& color, float frames_count) const;
 };
