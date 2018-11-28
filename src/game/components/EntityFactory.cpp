ab2dc82950c69f23d1adec7c6a61f9ac26658ecd
Artyom Yazinsky <iw2snl@gmail.com>
2018-11-29 00:47:37 +0200

    Use a generation entity factory

--- src/game/components/EntityFactory.cpp
+++ src/game/components/EntityFactory.cpp
@@ -3,9 +3,36 @@
 #include <cassert>
 
 Entity EntityFactory::create_entity() {
-    assert(_id_counter < std::numeric_limits<Entity::Id>::max() && "Entity pool exhausted");
+    const std::size_t MINIMUM_FREE_INDICES = 1024;
+    const std::size_t GENERATION_ZERO = 0;
 
-    auto entity = Entity{_id_counter};
-    ++_id_counter;
-    return entity;
+    Entity::Index index;
+    // If there are not yet enough entities to reuse
+    if (_free_indices.size() <= MINIMUM_FREE_INDICES) {
+        // create a new entity with generation zero (0)
+        _generations.push_back(GENERATION_ZERO);
+        // return it's generation index
+        index = _generations.size() - 1;
+
+    } else {
+        // else if a number of entities can be reused
+        // take the oldest entity and pop it
+        index = _free_indices.front();
+        _free_indices.pop_front();
+    }
+
+    return Entity{index, _generations[index]};
+}
+
+bool EntityFactory::is_alive(Entity entity) const {
+    // If given entity and stored entities generations match, the entity is alive.
+    return _generations.size() > entity._index
+           && _generations[entity._index] == entity._generation;
+}
+
+void EntityFactory::destroy(Entity entity) {
+    // To invalidate an entity, increase the generation stored by it's index, also store the index as free for reuse
+    auto index = entity._index;
+    ++_generations[index];
+    _free_indices.push_back(index);
 }
--- src/game/components/EntityFactory.h
+++ src/game/components/EntityFactory.h
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include <unordered_set>
+#include <vector>
+#include <deque>
 
 class EntityFactory;
 
@@ -10,28 +12,32 @@ public:
     Entity() = default;
 
     friend bool operator==(const Entity& left, const Entity& right) {
-        return left._id == right._id;
+        return left._index == right._index
+               && left._generation == right._generation;
     }
 
 private:
-    using Id = std::int32_t;
+    using Index = std::uint32_t;
+    using Generation = std::uint32_t;
 
-    explicit Entity(Id id) : _id{id} {
+    explicit Entity(Index index, Generation generation) : _index{index}, _generation{generation} {
     }
 
     friend class EntityFactory;
 
     friend struct std::hash<Entity>;
 
-    Id _id = 0;
+    Index _index = 0;
+    Generation _generation = 0;
 };
 
 namespace std {
     template <>
     struct hash<Entity> {
         inline size_t operator()(const Entity& entity) const {
-            hash<Entity::Id> hasher{};
-            return hasher(entity._id);
+            auto hashed_index = hash<Entity::Index>{}(entity._index);
+            auto hashed_generation = hash<Entity::Generation>{}(entity._generation);;
+            return hashed_index ^ (hashed_generation << 1);
         }
     };
 }
@@ -40,6 +46,11 @@ class EntityFactory {
 public:
     Entity create_entity();
 
+    bool is_alive(Entity entity) const;
+
+    void destroy(Entity entity);
+
 private:
-    Entity::Id _id_counter;
+    std::vector<Entity::Generation> _generations;
+    std::deque<Entity::Index> _free_indices;
 };
